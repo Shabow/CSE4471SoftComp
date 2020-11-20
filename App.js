90d173518ebad9a5e1ede50537867dae56d3090d
shabow <bower.205@osu.edu>
2020-11-20 00:13:57 -0500

    Alerts

--- App.js
+++ App.js
@@ -1,35 +1,73 @@
 // App.js
 
-import React, { useState } from 'react'
-import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Alert, StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native'
 import {REACT_APP_IPSTACK_ACCESS_KEY, REACT_APP_POSSTACK_ACCESS_KEY} from '@env'
 //import Geolocation from '@react-native-community/geolocation'
 import GetLocation from 'react-native-get-location'
 import { NetworkInfo } from "react-native-network-info"
 import DeviceInfo from 'react-native-device-info'
 
+const MY_IP = "65.24.247.147"
+
 export default function App() {
-
   const [loading, setLoading] = useState(true)
-
-  const [city, setCity] = useState()
-
-  const [region_code, setRegionCode] = useState()
-
+  const [city, setCity] = useState('UNKNOWN');
+  const [region, setRegion] = useState('UNKNOWN');
   const [ipAddress, setIPAddress] = useState()
-
   const [deviceName, setDeviceName] = useState()
-
   const [deviceType, setDeviceType] = useState()
-
   const [isEmulator, setIsEmulator] = useState()
-
-  const myIP = "65.24.247.147"
 
   // Get Local IP
   NetworkInfo.getIPAddress().then(ip => {
-    setIPAddress(myIP)
+    setIPAddress(MY_IP)
   });
+
+  useEffect(() => {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+      maximumAge: 1000
+    })
+        .then(position => {
+          if (position) {
+            const POS_KEY = REACT_APP_POSSTACK_ACCESS_KEY
+
+            let lat = position.latitude
+            let long = position.longitude
+
+            const posResponse = 'http://api.positionstack.com/v1/reverse?access_key='
+                +POS_KEY
+                +'&query='
+                +lat
+                +','
+                +long
+                +'&limit=1&fields=results.locality,results.region'
+
+            fetch(posResponse)
+                .then(response => response.json())
+                .then(data => {
+                  if (data) {
+                    console.log(data)
+                    if (data.data[0].locality == null) {
+                      setCity(data.data[0].administrative_area)
+                    } else {
+                      setCity(data.data[0].locality)
+                    }
+
+                    setRegion(data.data[0].region_code)
+                  }
+
+                  setLoading(false)
+                })
+          }
+        })
+        .catch(error => {
+          const { code, message } = error;
+          console.log(code, message);
+        })
+  }, []);
 
   /*const IP_KEY = REACT_APP_IPSTACK_ACCESS_KEY
 
@@ -54,47 +92,48 @@ export default function App() {
     setDeviceType(type)
   });
 
-  function getLocation() {
-    GetLocation.getCurrentPosition({
-      enableHighAccuracy: true,
-      timeout: 15000,
-      maximumAge: 1000
-    })
-        .then(position => {
-          if (position != undefined) {
-            const POS_KEY = REACT_APP_POSSTACK_ACCESS_KEY
+  function fraudAlert() {
+    Alert.alert('This request has been reported', '', [
+      {
+        text: "Ok",
+        onPress: () => {},
+      },
+    ])
+  }
 
-            let lat = position.latitude
-            let long = position.longitude
+  function genericAlert(title) {
+    Alert.alert(title, '', [
+      {
+        text: "Ok",
+        onPress: () => {},
+      },
+    ])
+  }
 
-            const posResponse = 'http://api.positionstack.com/v1/reverse?access_key='
-                +POS_KEY
-                +'&query='
-                +lat
-                +','
-                +long
-                +'&limit=1&fields=results.locality,results.region'
+  function mistakeAlert() {
+    Alert.alert('Why are you denying this request?', '', [
+      {
+        text: "It was a mistake",
+        onPress: () => {},
+      },
+      {
+        text: "It seemed fraudulent",
+        onPress: fraudAlert,
+      },
+    ])
+  }
 
-            fetch(posResponse)
-                .then(response => response.json())
-                .then(data => {
-                  if (data != undefined) {
-                    console.log(data)
-                    if (data.data[0].locality == null) {
-                      setCity(data.data[0].administrative_area)
-                    } else {
-                      setCity(data.data[0].locality)
-                    }
-                    setRegionCode(data.data[0].region_code)
-                  }
-                  setLoading(false)
-                })
-          }
-        })
-        .catch(error => {
-          const { code, message } = error;
-          console.log(code, message);
-        })
+  function displayAlert(title, callback) {
+    Alert.alert(title, `${city}, ${region}`, [
+      {
+        text: "Approve",
+        onPress: callback,
+      },
+      {
+        text: "Deny",
+        onPress: mistakeAlert,
+      },
+    ])
   }
 
   const LocalNotification = () => {
@@ -113,22 +152,49 @@ export default function App() {
     })
   }
 
+  if (loading) {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Hello</Text>
+        </View>
+    )
+  }
+
   return (
       <View style={styles.container}>
-        <TouchableOpacity onPress={getLocation}>
+{/*      <TouchableOpacity>
           <Text style={styles.welcome}>Device Info</Text>
           <Text style={styles.info}>Location: {loading ? "Press Me" : `${city}, ${region_code}`}</Text>
           <Text style={styles.info}>IP Address: {ipAddress}</Text>
           <Text style={styles.info}>Device Name: {deviceName}</Text>
           <Text style={styles.info}>Device Type: {deviceType}</Text>
           <Text style={styles.info}>isEmulator? {isEmulator ? "True" : "False"}</Text>
-        </TouchableOpacity>
+        </TouchableOpacity>*/}
+        <View style={styles.buttons}>
+          <View style={styles.button}>
+          <Button
+              onPress={() => displayAlert("Unlock Car", () => genericAlert('Car unlocked'))}
+              title={"Unlock Car"}
+              color={"#841584"}
+              accessibilityLabel="Press to send request to unlock car"
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+              onPress={() => displayAlert("Start Car", () => genericAlert('Car started'))}
+              title={"Start Car"}
+              color={"#142253"}
+              accessibilityLabel="Press to send request to start car"
+          />
+        </View>
+        </View>
       </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -143,5 +209,16 @@ const styles = StyleSheet.create({
     fontSize: 25,
     textAlign: 'center',
     margin: 10
+  },
+  buttons: {
+    height: 75,
+    textAlign: 'center',
+    marginTop: 30,
+    flexDirection: 'row',
+  },
+  button: {
+    height: 75,
+    width: 150,
+    margin: 20,
   }
 })
