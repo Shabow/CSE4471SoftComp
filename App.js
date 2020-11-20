9fbb82145ca296c56fff423309384fa1a8d6e57c
shabow <bower.205@osu.edu>
2020-11-19 22:30:19 -0500

    Clean Original

--- App.js
+++ App.js
@@ -1,114 +1,147 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+// App.js
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { useState } from 'react'
+import { Alert, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import {REACT_APP_IPSTACK_ACCESS_KEY, REACT_APP_POSSTACK_ACCESS_KEY} from '@env'
+//import Geolocation from '@react-native-community/geolocation'
+import GetLocation from 'react-native-get-location'
+import { NetworkInfo } from "react-native-network-info"
+import DeviceInfo from 'react-native-device-info'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
 
-const App: () => React$Node = () => {
+  const [loading, setLoading] = useState(true)
+
+  const [city, setCity] = useState()
+
+  const [region_code, setRegionCode] = useState()
+
+  const [ipAddress, setIPAddress] = useState()
+
+  const [deviceName, setDeviceName] = useState()
+
+  const [deviceType, setDeviceType] = useState()
+
+  const [isEmulator, setIsEmulator] = useState()
+
+  const myIP = "65.24.247.147"
+
+  // Get Local IP
+  NetworkInfo.getIPAddress().then(ip => {
+    setIPAddress(myIP)
+  });
+
+  /*const IP_KEY = REACT_APP_IPSTACK_ACCESS_KEY
+
+  const ipResponse = 'http://api.ipstack.com/'+ipAddress+'?access_key='+IP_KEY
+
+  fetch(ipResponse)
+      .then(response => response.json())
+      .then(data => console.log(data));*/
+
+  DeviceInfo.getDeviceName().then(name => {
+    // iOS: "Becca's iPhone 6"
+    // Android: ?
+    // Windows: ?
+    setDeviceName(name)
+  });
+
+  DeviceInfo.isEmulator().then(value => {
+    setIsEmulator(value)
+  });
+
+  DeviceInfo.getDevice().then(type => {
+    setDeviceType(type)
+  });
+
+  function getLocation() {
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 15000,
+      maximumAge: 1000
+    })
+        .then(position => {
+          if (position != undefined) {
+            const POS_KEY = REACT_APP_POSSTACK_ACCESS_KEY
+
+            let lat = position.latitude
+            let long = position.longitude
+
+            const posResponse = 'http://api.positionstack.com/v1/reverse?access_key='
+                +POS_KEY
+                +'&query='
+                +lat
+                +','
+                +long
+                +'&limit=1&fields=results.locality,results.region'
+
+            fetch(posResponse)
+                .then(response => response.json())
+                .then(data => {
+                  if (data != undefined) {
+                    console.log(data)
+                    if (data.data[0].locality == null) {
+                      setCity(data.data[0].administrative_area)
+                    } else {
+                      setCity(data.data[0].locality)
+                    }
+                    setRegionCode(data.data[0].region_code)
+                  }
+                  setLoading(false)
+                })
+          }
+        })
+        .catch(error => {
+          const { code, message } = error;
+          console.log(code, message);
+        })
+  }
+
+  const LocalNotification = () => {
+    PushNotification.localNotification({
+      autoCancel: true,
+      bigText:
+          'This is local notification demo in React Native app. Only shown, when expanded.',
+      subText: 'Local Notification Demo',
+      title: 'Local Notification Title',
+      message: 'Expand me to see more',
+      vibrate: true,
+      vibration: 300,
+      playSound: true,
+      soundName: 'default',
+      actions: '["Yes", "No"]'
+    })
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={getLocation}>
+          <Text style={styles.welcome}>Device Info</Text>
+          <Text style={styles.info}>Location: {loading ? "Press Me" : `${city}, ${region_code}`}</Text>
+          <Text style={styles.info}>IP Address: {ipAddress}</Text>
+          <Text style={styles.info}>Device Name: {deviceName}</Text>
+          <Text style={styles.info}>Device Type: {deviceType}</Text>
+          <Text style={styles.info}>isEmulator? {isEmulator ? "True" : "False"}</Text>
+        </TouchableOpacity>
+      </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  welcome: {
+    fontSize: 50,
+    textAlign: 'center',
+    margin: 10
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+  info: {
+    fontSize: 25,
+    textAlign: 'center',
+    margin: 10
+  }
+})
